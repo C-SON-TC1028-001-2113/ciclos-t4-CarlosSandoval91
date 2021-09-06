eea88ecf3d78b27f1c97376e0342945ef0587155
CarlosSandoval91 <A01252494@itesm.mx>
2021-09-06 20:19:50 +0000

    pares

--- assignments/19ParesRangoFunc/src/exercise.py
+++ assignments/19ParesRangoFunc/src/exercise.py
@@ -1,6 +1,20 @@
 
 def main():
-
+    num = int(input("Valor 1: ")) 
+    nim = int(input("Valor 2: ")) 
+    if num>0 and nim>0:
+        if nim!=num:
+            if num>nim:
+                for i in range (nim,num+1):
+                    if i%2==0:
+                        print(i)
+                
+            elif nim>num:
+                for i in range (num,nim+1):
+                    if i%2==0:
+                        print(i)
+        else:
+            print('No hay pares')
 
 if __name__=='__main__':
     main()
